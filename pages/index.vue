46aeb7a7c3fd1c3081f07da883e81aad1cbc3536
Badar Usman <baderusman23@gmail.com>
2022-05-22 00:09:40 +0400

    Added Tailwind css

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto bg-gray-300 p-8">
+    <h1 class="font-bold text-gray-600 text-lg">{{ appVersion }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+// Component state
+const appVersion: Ref<String> = ref('0.0.1-alpha')
+
+
+</script>
+
+<style scoped>
+
+</style>
